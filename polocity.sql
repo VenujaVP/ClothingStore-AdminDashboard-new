3ddb53738f32cae3e2b5557f7f399534bd8dea70
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-05-20 23:41:13 +0530

    Update generation time in SQL dump

--- polocity.sql
+++ polocity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2025 at 07:25 PM
+-- Generation Time: May 20, 2025 at 08:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
